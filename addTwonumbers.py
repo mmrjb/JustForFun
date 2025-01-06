562de9b441be2496e90f8cee4bb7f66839595888
mmrjb <rajabzadeh01@gmail.com>
2025-01-06 09:34:50 +0330

    addTwoNumbers

---
+++ addTwonumbers.py
@@ -0,0 +1,10 @@
+#add two numbers
+#imput numbers
+num_1 = input("please enter the first number : ")
+num_2 = input("please enter the second number : ")
+
+#Sum 
+addNumber = float(num_1) + float (num_2)
+
+#result
+print(f"result of {num_1} + {num_2} is {addNumber}")
