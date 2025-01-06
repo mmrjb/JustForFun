84929fa5ea0e96f02b86609c3c3f371c2bdbb9c7
mmrjb <rajabzadeh01@gmail.com>
2025-01-06 09:36:09 +0330

    findTheSquareRoot

---
+++ findTheSquareRoot.py
@@ -0,0 +1,9 @@
+#find the square root of a number 
+#input number
+number = float(input("please enter the number you want to now the square root : "))
+
+#if you find the power 1/2 of a number you have the root of the number
+
+result = number ** 0.5 
+
+print (f"The square root of {number} is {result}")
