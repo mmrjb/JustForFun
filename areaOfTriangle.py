44ccc852bcd1affb23ed6c2ad1b64527f73c4fbf
mmrjb <rajabzadeh01@gmail.com>
2025-01-06 09:35:08 +0330

    areaOfTriangle

---
+++ areaOfTriangle.py
@@ -0,0 +1,10 @@
+#area of a triangle 
+# we can fine the area of a triangle with the following formula :
+# A = H*B /2 
+
+H = float (input ("Please enter the height of the triangle in centimeters: ")) 
+B = float (input ("Please enter the base of the triangle in centimeters: "))
+
+A = (H*B) / 2 
+
+print(f"The area of the triangle is {A} Square centimeters (cm2) " )
